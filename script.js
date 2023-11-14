e66988055ccd484ef51043524026654cd474a2d8
rafaelabarbieric <barbieridacruzrafaela@gmail.com>
2023-11-14 16:55:41 -0300

    add informaçãoes e idiomas na calculadora

--- script.js
+++ script.js
@@ -1,27 +1,69 @@
-function clq1() {
-    alert("Você clicou no Botão 1");
-}
-
-let bt2 = document.getElementById("bt2");
-bt2.addEventListener("click", clq2);
-
-function clq2() {
-    alert("Você clicou no Botão 2");
-}
-
+let ttl = document.querySelector("#titulo");
 let valor1 = document.querySelector("#valor1");
 let valor2 = document.querySelector("#valor2");
+let soma = document.querySelector("#btSoma");
+let sub = document.querySelector("#btSub");
+let mult = document.querySelector("#btMult");
+let div = document.querySelector("#btDiv");
 let resultado = document.querySelector("#resultado");
 
-valor1.addEventListener("keyup", function() {
+soma.addEventListener("click", function(){
     somar(Number(valor1.value), Number(valor2.value));
-});
-
-valor2.addEventListener("keyup", function() {
-    somar(Number(valor1.value), Number(valor2.value));
-});
+})
 
 function somar(a, b) {
     let soma = a + b;
     resultado.innerHTML = soma;
 }
+
+sub.addEventListener("click", function(){
+    subtrair(Number(valor1.value), Number(valor2.value));
+})
+
+function subtrair(a, b) {
+    let sub = a - b;
+    resultado.innerHTML = sub;
+}
+
+mult.addEventListener("click", function(){
+    multiplicar(Number(valor1.value), Number(valor2.value));
+})
+
+function multiplicar(a, b) {
+    let mult = a * b;
+    resultado.innerHTML = mult;
+}
+
+div.addEventListener("click", function(){
+    dividir(Number(valor1.value), Number(valor2.value));
+})
+
+function dividir(a, b) {
+    let div = a / b;
+    resultado.innerHTML = div;
+}
+
+let selectedLanguage = document.querySelector("#idioma");
+selectedLanguage.addEventListener("change", function() {
+    if (this.value == "pt") {
+        ttl.textContent = "Calculadora";
+        soma.value = "Soma";
+        sub.value = "Subtração";
+        mult.value = "Multiplicação";
+        div.value = "Divisão";
+    }
+    else if (this.value == "en") {
+        ttl.textContent = "Calculator";
+        soma.value = "Sum";
+        sub.value = "Subtraction";
+        mult.value = "Multplication";
+        div.value = "Division";
+    }
+    else if (this.value == "es") {
+        ttl.textContent = "Calculadora";
+        soma.value = "Suma"
+        sub.value = "Sustracción";
+        mult.value = "Multiplicación";
+        div.value = "División";
+    }
+})
